ca3efb21cfcf9dad28196d4ec4f8b6120ec2322d
cdominick091984 <cdominick091984@gmail.com>
2020-07-09 12:51:58 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def new
+  end
+
+  def create
+    @student_first_name = params[:student][:first_name]
+  end
+
+  private 
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
